2f3f4d853b6c1815094a3ad1698a8b9ae9e44b0e
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-03 11:31:02 -0500

    bonus percentage calculation

--- client.js
+++ client.js
@@ -7,6 +7,50 @@ var mayella = { name: "Mayella", employeeNumber: "89068", annualSalary: "35000",
 
 var employees = [ atticus, jem, boo, scout, robert, mayella ];
 
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+//YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+// var Mockingbird = function(name, employeeNumber, annualSalary, reviewRating) {
+//   this.name = name;
+
+  //this.
+  var bonusPercentage = function(reviewRating, employeeNumber, annualSalary)  {
+   bonusPercentage = 0;
+// rating bonus
+  var ratingBonus = 0;
+  if(reviewRating === 3) {
+    ratingBonus = 0.04;
+  } else if (reviewRating === 4 ) {
+    ratingBonus = 0.06;
+  } else if (reviewRating === 5){
+    ratingBonus = 0.1;
+  }
+  else {
+    ratingBonus = 0;
+  }
+// time bonus
+var timeBonus = 0;
+    if(employeeNumber.length <= 4) {
+    timeBonus = 0.05;
+  }
+  // salary penalty
+  var salaryPenalty = 0;
+  if(parseInt(annualSalary) > 65000 ) {
+    salaryPenalty -= 0.01;
+  }
+
+  bonusPercentage = ratingBonus + timeBonus + salaryPenalty;
+
+  if(bonusPercentage > 0.13) {
+    bonusPercentage = 0.13;
+  } else if(bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+  return bonusPercentage;
+};//end checkBonus
+
+//   this.totalCompensation;
+//   this.totalBonus;
+//};
+
+console.log('bonus percentage: ', bonusPercentage(4, '77777', '35000'));
 
 console.log(employees);
